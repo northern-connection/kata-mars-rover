53c3213bd6f54183cada11d7eba12717d2c2e74b
Antonio de la Torre <antonio@codesai.com>
2018-03-15 10:44:05 +0100

    Initial commit

---
+++ src/test/java/MarsRoverTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MarsRoverTest {
+
+    @Test
+    public void fixme() {
+        assertThat(true, is(false));
+    }
+}
