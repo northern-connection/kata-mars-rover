6cfc40e244be7ba41f1455c918bc11cc6b9a133e
Antonio de la Torre <antondelatorre@gmail.com>
2018-05-15 23:51:00 +0200

    Refactor: Remove x and y properties.

--- src/main/java/MarsRover/Rover.java
+++ src/main/java/MarsRover/Rover.java
@@ -3,8 +3,6 @@ package MarsRover;
 import java.util.Objects;
 
 class Rover {
-    private int x;
-    private int y;
     private Point point;
     private Direction newDirection;
 
@@ -51,21 +49,5 @@ class Rover {
     public int hashCode() {
 
         return Objects.hash(point, newDirection);
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
     }
 }
